d8a4bdf4ebb0cb29a27a6bf1e85e2f0f2681ea49
Fredy-SSA <alin@zizo.ro>
2020-08-04 09:23:35 +0300

    all

--- Ziua_2/Comenzi.ps1
+++ Ziua_2/Comenzi.ps1
@@ -1,1 +1,24 @@
-﻿
+﻿# Obiect
+Test-Connection 
+
+ping localhost -t
+
+Test-Connection  localhost -Count 1 | select *  # select * iti arata toate propietatile cu valoare
+Test-Connection "." | gm
+
+Get-Service | gm
+
+(Get-Service XblGameSave).Start()
+(Get-Service XblGameSave).Stop()
+
+$service = Get-Service XblGameSave
+$service.Start()
+$service.stop()
+
+Get-Service XblGameSave 
+
+
+
+
+help Test-Connection -ShowWindow  
+help Test-Connection -example

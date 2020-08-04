5e0bd051132a98534eccc7a4c1fd6cf01d7db116
Fredy-SSA <alin@zizo.ro>
2020-08-04 09:42:22 +0300

    all

--- Ziua_2/Comenzi.ps1
+++ Ziua_2/Comenzi.ps1
@@ -4,10 +4,13 @@ Test-Connection
 ping localhost -t
 
 Test-Connection  localhost -Count 1 | select *  # select * iti arata toate propietatile cu valoare
-Test-Connection "." | gm
+Test-Connection "." | gm # Get-member
+
+Test-Connection  localhost -Count 1 
 
 Get-Service | gm
 
+#apelare metoda
 (Get-Service XblGameSave).Start()
 (Get-Service XblGameSave).Stop()
 
@@ -22,3 +25,18 @@ Get-Service XblGameSave
 
 help Test-Connection -ShowWindow  
 help Test-Connection -example
+
+Get-Command *update*
+
+help Get-WindowsUpdateLog -ShowWindow
+Get-Command -Module WindowsUpdate
+
+Get-Command *fix*
+
+help Get-HotFix  -ShowWindow
+
+Get-HotFix | select *
+Get-HotFix | gm
+
+Get-Module -ListAvailable
+

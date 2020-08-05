63dd2b717860900eefbe515489ae01f65c39a117
Fredy-SSA <alin@zizo.ro>
2020-08-05 16:32:08 +0300

    all

---
+++ Ziua_3/replace_lista.csv.ps1
@@ -0,0 +1,8 @@
+﻿#Get-ChildItem C:\Alin\PS-01\Ziua_3 | ? name -like "*.csv" | select BaseName
+
+
+$path = Get-ChildItem C:\Alin\PS-01\Ziua_3 | ? name -like "*.csv" 
+
+$path.name | % {
+($_).replace(".csv","")
+}

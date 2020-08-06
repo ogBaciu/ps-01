778c202de7df439eaad9782cffa14857ab4ac027
Fredy-SSA <alin@zizo.ro>
2020-08-06 11:15:28 +0300

    all

---
+++ Ziua_4/CustomLogScript.ps1
@@ -0,0 +1,8 @@
+﻿[datetime]$zi = "08/03/2020" 
+
+$zi.ToShortDateString()
+
+Get-EventLog -LogName Application | select -First 1 | select *
+
+Get-EventLog -LogName Application | ? {$_.TimeGenerated -le $zi.AddDays(1) -and $_.TimeGenerated -ge $zi  }
+

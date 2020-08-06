ae5dd252e185a1e7267fb6a3f8069747b2b5e48b
Fredy-SSA <alin@zizo.ro>
2020-08-06 14:21:20 +0300

    all

---
+++ Ziua_4/functie get-service.ps1
@@ -0,0 +1,10 @@
+﻿#FUnctie sa ne arate doar running sau stopped si ultimele  intrari sa le poti alege / ca nr
+#get-service
+
+function servicii ($status, $last)
+{
+Get-Service | ? status -like "*$status*" | select -Last $last
+    
+}
+
+servicii -status st -last 8

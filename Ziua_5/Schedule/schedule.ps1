a4c2dc7818f03b88b774ddf6b4dec51a808fb478
Fredy-SSA <alin@zizo.ro>
2020-08-07 16:32:53 +0300

    all

--- Ziua_5/Schedule/schedule.ps1
+++ Ziua_5/Schedule/schedule.ps1
@@ -1,1 +1,18 @@
-﻿
+﻿Get-Command *schedule*
+
+Get-Command -Module ScheduledTasks
+
+$trigger = New-JobTrigger –Once –At (Get-Date).AddMinutes(2)
+
+Register-ScheduledJob –Trigger $trigger –Name DemoJob –ScriptBlock { Get-EventLog –LogName Application }
+
+Get-ScheduledJob | Select –Expand JobTriggers 
+
+Get-ScheduledJob 
+
+
+Get-Job
+
+Receive-Job –Name DemoJob -Keep
+
+Get-Job –Name DemoJob | Remove-Job

8f31b6cdd5d0686d48cce991b59bfa1473858c5a
Fredy-SSA <alin@zizo.ro>
2020-08-07 09:42:26 +0300

    all

---
+++ Ziua_5/modul-copy/modul-copy.psm1
@@ -0,0 +1,42 @@
+<#
+.Synopsis
+   Short description
+.DESCRIPTION
+   Long description
+.EXAMPLE
+   Example of how to use this cmdlet
+.EXAMPLE
+   Another example of how to use this cmdlet
+#>
+function Copy-Module
+{
+    [CmdletBinding()]
+    [Alias()]
+    [OutputType([int])]
+    Param
+    (
+        # Param1 help description
+        [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=0)]
+        $Param1,
+
+        # Param2 help description
+        [int]
+        $Param2
+    )
+
+    Begin
+    {
+
+    }
+    Process
+    {
+
+     Robocopy /?
+
+    }
+    End
+    {
+    }
+}
---
+++ Ziua_5/modul-functii/modul-functii.psm1
@@ -0,0 +1,15 @@
+﻿function Test-Function
+{
+ param($Parameter1=’Default Value1’, $Parameter2=’Default Value2’)
+ “You entered $Parameter1 and $Parameter2”
+} 
+
+function Test-Me {
+ param
+ (
+ [Parameter(Mandatory=$true, HelpMessage=’Enter your name please’)]
+ $name
+ )
+ “You entered $name.”
+}
+

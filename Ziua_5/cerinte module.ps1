e0d2f6179a30b9a0a878d2bd1f2243b024cb3211
Fredy-SSA <alin@zizo.ro>
2020-08-07 11:30:00 +0300

    all

---
+++ Ziua_5/cerinte module.ps1
@@ -0,0 +1,10 @@
+﻿1 - meniu 
+
+5 -Topicuri
+ -Get-eventlog Application error,warning
+ -Get-Service  
+ -get-hotfix
+ -test daca merge netu 
+  Test-NetConnection
+ -count cate module ai instalate C:\Program Files\WindowsPowerShell\Modules 
+  | Measure
